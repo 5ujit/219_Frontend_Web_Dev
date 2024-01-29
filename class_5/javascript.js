0fe93b5c1b9ad94c3b4298babd48256ff6f8132d
5ujit <149666757+5ujit@users.noreply.github.com>
2024-01-29 11:06:55 +0530

    class

---
+++ class_5/javascript.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+
+
+
+let a = "Hello learnners";
+let b = "joining";
+let c = "12";
+let d = b + c;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+let e =5;
+let f =10;
+console.log("e+f" +  (e + f));
+console.log("e-f" +  (e - f));
+console.log("e*f" +  (e * f));
+console.log("e/f" +  (e / f));
+
+
+
+
