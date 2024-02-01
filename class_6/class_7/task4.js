8a8df81414c76a943a1fb00cc38c3907be2b9b90
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-01 13:31:46 +0530

    js class

---
+++ class_6/class_7/task4.js
@@ -0,0 +1,5 @@
+let fruits = ['apple' , 'banana', 'charry'];
+  for(let i= 0; i <  fruits.length; i++  ){
+    console.log(fruits [i])
+
+  }
---
+++ class_6/task2.js
@@ -0,0 +1,23 @@
+let score = parseInt(prompt("enter the "));
+if(score <= 90 ){
+  console.log("grade A")
+}
+else if(score =>80 && score<=89 ) {
+  console.log("Grade B")
+}
+else if(score =>80 && score<=89 ) {
+  console.log("Grade C")
+}
+else if(score =>70 && score<=79 ) {
+  console.log("Grade D")
+}
+else if(score =>60 && score<=69 ) {
+  console.log("Grade E")
+}
+else if(score => 60 ) {
+  console.log("Grade B")
+}
+
+
+
+
