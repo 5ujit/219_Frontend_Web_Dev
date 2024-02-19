2ed598af33269e43798b5d9eb62196a40a656b7a
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-19 18:50:31 +0530

    frontend

---
+++ class_6/task5_.js
@@ -0,0 +1,6 @@
+let productName = ['apple', 'charry', 'banana'];
+let productquantity = [10, 20, 40];
+let productprice = [68, 45, 60];
+for(let i = 0; i<=productName.values; i++    ){
+  console.log(...productprice ,...productName)
+}
---
+++ class_7/task4.js
@@ -0,0 +1,6 @@
++
+let fruits = ['apple' , 'banana', 'charry'];
+  for(let i= 0; i <  fruits.length; i++  ){
+    console.log(fruits [i])
+
+  }
