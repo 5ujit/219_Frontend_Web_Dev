a27bec664c731398962901c43d02834da097e93e
5ujit <149666757+5ujit@users.noreply.github.com>
2024-02-28 19:39:03 +0530

    219

---
+++ Rivision/arrowFunction.js
@@ -0,0 +1,20 @@
+function nice(name){
+  console.log("Hey " + name+" you are nice !");
+  console.log("Hey " + name+" you good!");
+  console.log("Hey " + name+" you !");
+  console.log("Hey " + name+" you are nice !");
+}
+nice("rohan")
+ 
+function sum(a,b){
+  return a + b
+}
+ var result= sum(5,10);
+console.log("The sum of these number is : " , result);
+
+const func1= (x)=>{
+    console.log("I am an arrow function ", x)
+
+}
+func1("34")
+
---
+++ Rivision/index.js
@@ -0,0 +1,15 @@
+// function nice(name){
+//   console.log("Hey " + name+" you are nice !");
+//   console.log("Hey " + name+" you good!");
+//   console.log("Hey " + name+" you !");
+//   console.log("Hey " + name+" you are nice !");
+// }
+// nice("rohan")
+ 
+function sum(a,b){
+  return a + b
+}
+ var result= sum(5,10);
+console.log("The sum of these number is : " , result);
+
+
